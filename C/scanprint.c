75d787bd5853d6ff1f93dfe31a2c4b5499c1bc31
戴　岩沢 <daiyanze@EverySense-Japan-no-MacBook-Air.local>
2017-02-28 17:40:53 +0900

    Modified folder structure

--- C/scanprint.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	float f;
-	printf("Enter a number\n");
-	scanf("%f", &f);
-	printf("The entered number is %f\n", f);
-}
--- C/union.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-
-union Data
-{
-	int i;
-	int j;
-	char s[30];
-};
